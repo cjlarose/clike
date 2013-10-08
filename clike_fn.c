8e1ba3c01fe5f711cca053b759b2df284da4a9e5
Chris La Rose <cjlarose@email.arizona.edu>
2013-10-07 22:50:29 -0700

    Stole TA's yyerror fn

--- clike_fn.c
+++ clike_fn.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
+#include <stdarg.h>
 #include "clike_fn.h"
 
+/*
 void yyerror (char *s, ... ) {
     fprintf(stderr, "%d: error: ", yylineno);
     return;
+}
+*/
+
+void yyerror(char *s, ...) {
+    va_list ap;
+    va_start(ap, s);
+
+    fprintf(stderr, "%d: error: ", yylineno);
+    vfprintf(stderr, s, ap);
+    fprintf(stderr, "\n");
 }
