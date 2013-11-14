8a1492559a7ffa9a0d534ad457896c4f4c2a5e93
Chris LaRose <cjlarose@gmail.com>
2013-11-13 21:14:24 -0700

    Added handy reminder comments

--- src/statements.c
+++ src/statements.c
@@ -2,20 +2,24 @@
 #include "array.h"
 #include "statements.h"
 
+// else may be null
 StmtNodeContainer *new_if_node(ExpNode *condition, StmtNodeContainer *then_stmt, 
     StmtNodeContainer *else_stmt) {
     return NULL;
 }
 
+// body may be null
 StmtNodeContainer *new_while_node(ExpNode *condition, StmtNodeContainer *body) {
     return NULL;
 }
 
+// all arguments may be null
 StmtNodeContainer *new_for_node(ExpNode *initialization, ExpNode *condition, 
     ExpNode *loop_expression, StmtNodeContainer *body) {
     return NULL;
 }
 
+// expr may be null
 StmtNodeContainer *new_return_node(ExpNode *expr) {
     return NULL;
 }
@@ -28,6 +32,7 @@ StmtNodeContainer *new_invocation_node(ExpNode *expr) {
     return NULL;
 }
 
+// stmts may be null
 StmtNodeContainer *new_block_node(Array *stmts) {
     return NULL;
 }
