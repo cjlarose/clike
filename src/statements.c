1d51a36fae304aa7489ce0ca5c04a03357cec54d
Chris LaRose <cjlarose@gmail.com>
2013-11-13 20:26:22 -0700

    Added stubs for statement methods

---
+++ src/statements.c
@@ -0,0 +1,32 @@
+#include "expressions.h"
+#include "array.h"
+
+StmtNodeContainer *new_if_node(ExpNode *condition, StmtNodeContainer *if_stmt, 
+    StmtNodeContainer *else_stmt) {
+    return NULL;
+}
+
+StmtNodeContainer *new_while_node(ExpNode *condition, StmtNodeContainer *body) {
+    return NULL;
+}
+
+StmtNodeContainer *new_for_node(ExpNode *initialization, ExpNode *condition, 
+    ExpNode *loop_expression, StmtNodeContainer *body) {
+    return NULL;
+}
+
+StmtNodeContainer *new_return_node(ExpNode *expr) {
+    return NULL;
+}
+
+StmtNodeContainer *new_assignment_node(ExpNode *expr) {
+    return NULL;
+}
+
+StmtNodeContainer *new_invocation_node(ExpNode *expr) {
+    return NULL;
+}
+
+StmtNodeContainer *new_block_node(Array *stmts) {
+    return NULL;
+}
