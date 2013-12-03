913945dc6760eb669f9f72dc35b853c4c6f4b780
Chris LaRose <cjlarose@gmail.com>
2013-12-02 22:27:21 -0700

    Added IR for if stmts

--- src/ir.c
+++ src/ir.c
@@ -172,7 +172,7 @@ Instruction *expr_to_ir(Env *env, ExpNode *expr, char **result_sym) {
                 evaluate,
                 goto_end,
                 true_label,
-                load_one_var,
+                load_one,
                 set_true,
                 end_label
             );
@@ -266,22 +266,84 @@ Instruction *expr_to_ir(Env *env, ExpNode *expr, char **result_sym) {
     return NULL;
 }
 
-void assg_stmt_to_ir(Env *env, AssignmentStatement *stmt) {
+Instruction *statement_to_ir(Env *env, StmtNodeContainer *stmt);
+
+Instruction *if_stmt_to_ir(Env *env, IfStatement *stmt) {
+    char *condition_var = next_tmp_symbol(env);
+    Instruction *condition = expr_to_ir(env, stmt->condition, &condition_var);
+    Instruction *then_stmt = statement_to_ir(env, stmt->then_stmt);
+    Instruction *else_stmt = NULL;
+    if (stmt->else_stmt)
+        else_stmt = statement_to_ir(env, stmt->else_stmt);
+
+    Instruction *true_label = label_instruction_new(next_tmp_symbol(env));
+    Instruction *end_label = label_instruction_new(next_tmp_symbol(env));
+
+    Instruction *jump_to_true = cond_jump_instruction_new(condition_var, 
+        true_label);
+    Instruction *jump_to_end = uncond_jump_instruction_new(end_label);
+
+    // for ifs with else stmts
+
+    // condition
+    // if (condition) goto true
+    // else code
+    // jump to end
+    // true:
+    // then code
+    // end:
+
+    printf("Condition: %p\n", condition);
+    printf("Jump To True: %p\n", jump_to_true);
+    printf("Else Stmt: %p\n", else_stmt);
+    printf("Jump To End: %p\n", jump_to_end);
+    printf("True Label: %p\n", true_label);
+    printf("Then Stmt: %p\n", then_stmt);
+    printf("End Label: %p\n", end_label);
+
+    if (else_stmt)
+        return concat_inst(
+            7,
+            condition,
+            jump_to_true,
+            else_stmt,
+            jump_to_end,
+            true_label,
+            then_stmt,
+            end_label
+        );
+    else
+        return concat_inst(
+            6,
+            condition,
+            jump_to_true,
+            jump_to_end,
+            true_label,
+            then_stmt,
+            end_label
+        );
+}
+
+Instruction *assg_stmt_to_ir(Env *env, AssignmentStatement *stmt) {
     //printf("ASSIGNMENT STMT!\n");
     //print_expr_ir(stmt->expr);
     Instruction *expr_inst = expr_to_ir(env, stmt->expr, NULL);
-    print_ir_list(expr_inst);
+    return expr_inst;
 }
 
-void statement_to_ir(Env *env, StmtNodeContainer *stmt) {
+Instruction *statement_to_ir(Env *env, StmtNodeContainer *stmt) {
     printf("PRINTING IR FOR STMT %p of type %d\n", stmt, stmt->type);
     switch (stmt->type) {
+        case IF_STMT:
+            return if_stmt_to_ir(env, &stmt->node.if_stmt);
+            break;
         case ASSIGNMENT_STMT:
-            assg_stmt_to_ir(env, &stmt->node.assg_stmt);
+            return assg_stmt_to_ir(env, &stmt->node.assg_stmt);
             break;
         default:
             break;
     }
+    return NULL;
 }
 
 void print_ir_procedure(Env *env, Array *stmts) {
@@ -290,7 +352,9 @@ void print_ir_procedure(Env *env, Array *stmts) {
     if (stmts)
         for (i = 0; i < stmts->length; i++) {
             StmtNodeContainer *stmt = *((StmtNodeContainer **) array_get(stmts, i));
-            statement_to_ir(env, stmt);
+            Instruction *result = statement_to_ir(env, stmt);
+            if (result)
+                print_ir_list(result);
         }
 }
 
