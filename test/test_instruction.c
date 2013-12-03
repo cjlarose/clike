400ad668dce297b0c43e4016d11f5be8fb666a3d
Chris LaRose <cjlarose@iplantcollaborative.org>
2013-12-03 11:05:02 -0700

    Added nested instruction test

--- test/test_instruction.c
+++ test/test_instruction.c
@@ -103,6 +103,36 @@ char *test_null_7() {
     return NULL;
 }
 
+char *test_nested_0() {
+    Instruction *inst1 = arithmetic_instruction_new();
+    Instruction *inst2 = copy_instruction_new("m1", "m2");
+    Instruction *inst3 = load_int_instruction_new(45);
+    Instruction *all = concat_inst(2, inst1, concat_inst(2, inst2, inst3));
+    mu_assert(inst_length(all) == 3, "Wrong size");
+    return NULL;
+}
+
+char *test_nested_1() {
+    Instruction *inst1 = arithmetic_instruction_new();
+    Instruction *inst2 = copy_instruction_new("m1", "m2");
+    Instruction *inst3 = load_int_instruction_new(45);
+    Instruction *all = concat_inst(2, concat_inst(2, inst1, inst2), inst3);
+    mu_assert(inst_length(all) == 3, "Wrong size");
+    return NULL;
+}
+
+char *test_nested_2() {
+    Instruction *inst1 = arithmetic_instruction_new();
+    Instruction *inst2 = copy_instruction_new("m1", "m2");
+    Instruction *inst3 = load_int_instruction_new(45);
+    Instruction *inst4 = load_float_instruction_new(45.0);
+    Instruction *all = concat_inst(2, 
+        concat_inst(2, concat_inst(2, inst1, inst2), inst3), 
+        inst4);
+    mu_assert(inst_length(all) == 4, "Wrong size");
+    return NULL;
+}
+
 char *all_tests() {
     mu_suite_start();
     mu_run_test(test_concat_0);
@@ -117,6 +147,9 @@ char *all_tests() {
     mu_run_test(test_null_5);
     mu_run_test(test_null_6);
     mu_run_test(test_null_7);
+    mu_run_test(test_nested_0);
+    mu_run_test(test_nested_1);
+    mu_run_test(test_nested_2);
     return NULL;
 }
 
