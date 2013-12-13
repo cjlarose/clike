00ab27441e8179f7c2965746c94db31da908319b
Chris LaRose <cjlarose@gmail.com>
2013-12-12 19:36:56 -0700

    Added empty print_procedure

--- src/mips.c
+++ src/mips.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "array.h"
 #include "env.h"
+#include "procedure.h"
 
 void print_data(void *k, void **v, void *info) {
     Env *env = info;
@@ -14,15 +15,15 @@ void print_data(void *k, void **v, void *info) {
     switch (sym->type) {
         case TYPE_CHAR:
         case TYPE_INT:
-            printf("%s: ", k);
+            printf("%s:", k);
             for (i = 0; i < length; i++)
-                printf(".word ");
+                printf(" .word");
             printf("\n");
             break;
         case TYPE_FLOAT:
-            printf("%s: ", k);
+            printf("%s:", k);
             for (i = 0; i < length; i++)
-                printf(".double ");
+                printf(" .double");
             printf("\n");
             break;
         default:
@@ -31,11 +32,19 @@ void print_data(void *k, void **v, void *info) {
 }
 
 void print_globals(Env *scope) {
-    printf(".data\n");
+    printf(".data\n\n");
     map_apply(&scope->table, &print_data, scope);
 }
 
+void print_procedure(Procedure *proc) {
+    printf("%s:\n", proc->id);
+}
+
 void print_mips(Env *global_scope, Array *procedures) {
-    printf("MIPS\n");
     print_globals(global_scope);
+    printf("\n");
+    printf(".text\n\n");
+    int i;
+    for (i = 0; i < procedures->length; i++)
+        print_procedure((Procedure *) array_get(procedures, i));
 }
